6c464003f01a77f3f63f6c8b547c72d0ba7cd9a5
jaden <jonghaha7@gmail.com>
2021-03-25 22:01:21 +0900

    :sparkles: 리뷰조회시 json 배열반환 -> 객체반환 수정

--- src/main/java/me/summerbell/muckit/reviews/ReviewController.java
+++ src/main/java/me/summerbell/muckit/reviews/ReviewController.java
@@ -2,11 +2,14 @@ package me.summerbell.muckit.reviews;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.Data;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import me.summerbell.muckit.accounts.auth.CurrentUser;
 import me.summerbell.muckit.domain.Account;
 import me.summerbell.muckit.utils.vo.RestaurantReviewVo;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -25,28 +28,31 @@ public class ReviewController {
     private final ObjectMapper objectMapper;
 
     @ExceptionHandler
-    public ResponseEntity reviewErrorHandler(NoSuchElementException exception){
+    public ResponseEntity<String> reviewErrorHandler(NoSuchElementException exception){
         return ResponseEntity.badRequest().body("등록된 리뷰가 없습니다.");
     }
 
     @ExceptionHandler
-    public ResponseEntity alreadyWrittenHandler(AlreadyWrittenException exception){
+    public ResponseEntity<String> alreadyWrittenHandler(AlreadyWrittenException exception){
         return ResponseEntity.badRequest().body(exception.getMessage());
     }
 
     @GetMapping("/api/restaurant-review")
-    public ResponseEntity getRestaurantReview(@RequestParam String restaurantKakaoId){
+    public ResponseEntity<Result> getRestaurantReview(@RequestParam String restaurantKakaoId){
 
         List<ReviewDto> review = reviewService.getReview(restaurantKakaoId);
+        Result result = new Result(review);
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(new MediaType(MediaType.APPLICATION_JSON));
 
-        return ResponseEntity.ok(review);
+        return new ResponseEntity<>(result, headers, HttpStatus.OK);
     }
 
     @PostMapping(value = "/api/restaurant-review",
             consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.MULTIPART_FORM_DATA_VALUE})
-    public ResponseEntity createRestaurantReview(@RequestPart String reviewInfo,
-                                                 @RequestParam Optional<MultipartFile> uploadImage,
-                                                 @CurrentUser Account account){
+    public ResponseEntity<ReviewDto> createRestaurantReview(@RequestPart String reviewInfo,
+                                                            @RequestParam Optional<MultipartFile> uploadImage,
+                                                            @CurrentUser Account account){
 
         // ios 로 배열밖에 넘기지 못해, 값 변환
         reviewInfo = reviewInfoConvert(reviewInfo);
@@ -68,6 +74,15 @@ public class ReviewController {
         reviewInfo = reviewInfo.replace("[", "{");
         reviewInfo = reviewInfo.replace("]","}");
         return reviewInfo;
+    }
+
+    @Data
+    static class Result{
+        private Object data;
+
+        public Result(Object data) {
+            this.data = data;
+        }
     }
 
 
